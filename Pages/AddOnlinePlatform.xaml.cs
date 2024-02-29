962375b77a530de9af8708566fbb5ad8f901b0b9
u31504 <u31504@P31504>
2024-02-29 13:09:43 +1000

    Добавьте файлы проекта.

---
+++ Pages/AddOnlinePlatform.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlinePlatforms_EX_StasIvan.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOnlinePlatform.xaml
+    /// </summary>
+    public partial class AddOnlinePlatform : Page
+    {
+        public AddOnlinePlatform()
+        {
+            InitializeComponent();
+        }
+
+        private void butAddPlatform_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnlineOrderHomesEntities.GetContext().OnlinePlatforms.Count(a => a.NamePlatform == NamePlatform.Text)>0)
+            {
+                MessageBox.Show("Такая платформа уже существует", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                OnlinePlatforms addPlatform = new OnlinePlatforms()
+                {
+                    NamePlatform = NamePlatform.Text,
+                    Site = NameSite.Text,
+                    CountUsers = int.Parse(CountPeoples.Text),
+                    Telephone = TextTelephon.Text,
+                    AuthorLogin = NameLoginAuthor.Text,
+                    id_Estate = int.Parse(idPlatform.Text)
+                };
+                OnlineOrderHomesEntities.GetContext().OnlinePlatforms.Add(addPlatform);
+                OnlineOrderHomesEntities.GetContext().SaveChanges();
+                MessageBox.Show("Данные заполнены", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                OnlineOrderHomesEntities.GetContext().OnlinePlatforms.ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Данные не заполнены", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void butback_Click(object sender, RoutedEventArgs e)
+        {
+            if (Manager.myFrame.CanGoBack)
+            {
+                Manager.myFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ Pages/OnlinePlatform.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlinePlatforms_EX_StasIvan.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OnlinePlatform.xaml
+    /// </summary>
+    public partial class OnlinePlatform : Page
+    {
+        public OnlinePlatform()
+        {
+            InitializeComponent();
+            GridOnlinePlatforms.ItemsSource = OnlineOrderHomesEntities.GetContext().OnlinePlatforms.ToList();
+        }
+
+        private void butAddState_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.myFrame.Navigate(new Pages.AddOnlinePlatform(null);
+        }
+
+        private void butDeleteState_Click(object sender, RoutedEventArgs e)
+        {
+            var DelPlatform = GridOnlinePlatforms.SelectedItems.Cast<OnlinePlatforms>().ToList();
+            try
+            {
+                if (MessageBox.Show($"Вы уверены что хотите удалить {DelPlatform.Count()} элементов?", "Ошибка", 
+                    MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    OnlineOrderHomesEntities.GetContext().OnlinePlatforms.RemoveRange(DelPlatform);
+                    OnlineOrderHomesEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                    GridOnlinePlatforms.ItemsSource = OnlineOrderHomesEntities.GetContext().OnlinePlatforms.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void butRedState_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.myFrame.Navigate(new Pages.AddOnlinePlatform((sender as Button).DataContext as OnlinePlatforms));
+        }
+    }
+}
